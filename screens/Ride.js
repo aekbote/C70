2d23ddc2212cbbbf7c53c7892ee7f1995a209210
aekbote <88759575+aekbote@users.noreply.github.com>
2022-08-10 12:56:47 -0700

    Update Ride.js

--- screens/Ride.js
+++ screens/Ride.js
@@ -86,12 +86,9 @@ export default class RideScreen extends Component {
             <TouchableOpacity
               style={styles.scanbutton}
 
-             // onPress={this.getCameraPermissions()}
-            // onPress={() => this.getCameraPermissions()}
-             // onPress={() => this.getCameraPermissions}
-             // onPress=() => this.getCameraPermissions()
-
-             
+            
+             onPress={() => this.getCameraPermissions()}
+            
 
             >
               <Text style={styles.scanbuttonText}>Scan</Text>
